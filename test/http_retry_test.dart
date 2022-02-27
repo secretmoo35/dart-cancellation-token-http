12eeeada23c74362125d2e93ca2ea00ce486b819
Jonathan Cole <JonathanPeterCole@gmail.com>
2022-02-27 18:12:58 +0000

    Remove unused import

--- test/http_retry_test.dart
+++ test/http_retry_test.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:cancellation_token/cancellation_token.dart';
 import 'package:cancellation_token_http/cancellable_http.dart';
 import 'package:cancellation_token_http/retry.dart';
 import 'package:cancellation_token_http/testing.dart';
